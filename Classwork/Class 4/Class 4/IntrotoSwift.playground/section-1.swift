6c8dc08f7fcbdb2a8db025ad1874c8ec96c1c201
Mary Sullivan <mary@sullivanville.com>
2015-01-05 20:25:03 -0500

    Saving changes before pulling code.

---
+++ Classwork/Class 4/Class 4/IntrotoSwift.playground/section-1.swift
@@ -0,0 +1,63 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//this is a comment line
+/*this is a comment block
+*/
+
+"This is a string. 8 is a number in a string and can't be computed."
+
+
+//Integers are whole numbers.
+
+5
+0
+10000
+
+// Doubles and floats are decimals
+54.76
+
+// Boolean 
+true
+false
+
+//arrays we will cover later, but they are linear collections of numbers rather like data in a book, where the data can only be referenced by its page number
+//dictionarys
+//both are collections
+
+//Variable: inferred  ie swift knew
+var myVariable = "Hello world"
+println(myVariable)
+
+var mySecondVariable: String = "Hello world again"
+mySecondVariable = "hello"
+
+var x = 10
+
+var y = 4
+
+x + y
+x * y
+//Not getting remainder bc inferred type was integer.
+x / y
+x - y
+x % y
+
+y = 20
+println(y)
+
+
+let myName = "Tedi"
+
+
+//myName "John" will error out because constants are immutable.
+
+let amIHere: String? = "hello"
+
+println(amIHere)
+//? sets optional and ! unwraps it
+
+
