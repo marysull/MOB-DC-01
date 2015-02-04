8bc5390cc29b12b7f4c3777b0512ffac7a9bf364
Mary Sullivan <mary@sullivanville.com>
2015-02-04 13:19:18 -0500

    commit including week 3 homework completed, 1:20 pm on 2/4

---
+++ Homework/Week 4/Delegation/Delegation/AddEventViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddEventViewController.swift
+//  Delegation
+//
+//  Created by Thomas Degry on 28/01/15.
+//  Copyright (c) 2015 Thomas Degry. All rights reserved.
+//
+
+import UIKit
+
+protocol EventProtocol {
+    func addEvent(newEvent:Event)
+}
+
+class AddEventViewController: UIViewController {
+
+    @IBOutlet var nameLabel: UITextField!
+    @IBOutlet var locationLabel: UITextField!
+    
+    var delegate:EventProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // add Cancel button on the left and save button on the right
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "dismiss")
+        let saveButton = UIBarButtonItem(title: "Save", style: UIBarButtonItemStyle.Plain, target: self, action: "save")
+
+        // set navigation items
+        self.navigationItem.title = "Add Event"
+        self.navigationItem.leftBarButtonItem = cancelButton
+        self.navigationItem.rightBarButtonItem = saveButton
+    }
+    
+    func save() {
+        let newEvent = Event(name: nameLabel.text, location: locationLabel.text)
+        self.delegate?.addEvent(newEvent)
+        dismiss()
+    }
+    
+    func dismiss() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
---
+++ Homework/Week 4/Delegation/Delegation/EventsTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  EventsTableViewController.swift
+//  Delegation
+//
+//  Created by Thomas Degry on 28/01/15.
+//  Copyright (c) 2015 Thomas Degry. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewController: UITableViewController, EventProtocol {
+    // event array
+    var arrEvents:[Event] = []
+    
+
+    @IBAction func addEvent(sender: UIBarButtonItem) {
+        let addEventViewController = self.storyboard?.instantiateViewControllerWithIdentifier("addEventVC") as AddEventViewController
+        addEventViewController.delegate = self
+        let navigationController = UINavigationController(rootViewController: addEventViewController)
+        self.presentViewController(navigationController, animated: true, completion: nil)
+    }
+    
+    func addEvent(newEvent: Event) {
+        println("foo")
+        self.arrEvents.append(newEvent)
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrEvents.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("eventCell", forIndexPath: indexPath) as UITableViewCell
+    
+    // Configure the cell...
+    let currentEvent = self.arrEvents[indexPath.row]
+    cell.textLabel.text = currentEvent.name
+    cell.detailTextLabel?.text = currentEvent.location
+    
+    return cell
+    }
+
+
+}
